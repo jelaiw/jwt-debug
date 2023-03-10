2c4f24c6c95a3683435a1fa980d902d33f12c1c6
jelaiw <14236583+jelaiw@users.noreply.github.com>
2023-03-10 02:26:57 +0000

    Rename tokens to words to improve readability.

--- jwt-debug.py
+++ jwt-debug.py
@@ -12,17 +12,17 @@ assert len(sys.argv) == 2
 jwt = sys.argv[1]
 print(jwt)
 
-tokens = jwt.split('.')
-assert len(tokens) == 3
+words = jwt.split('.')
+assert len(words) == 3
 
 # Decode and parse header into a dict.
-header = json.loads(base64.urlsafe_b64decode(tokens[0]))
+header = json.loads(base64.urlsafe_b64decode(words[0]))
 print(json.dumps(header, indent=4))
 
 # Decode and parse payload into a dict.
 # See https://stackoverflow.com/a/49459036.
 # Also, see https://gist.github.com/perrygeo/ee7c65bb1541ff6ac770.
-payload = json.loads(base64.urlsafe_b64decode(tokens[1] + '==')) # Kludge for incorrect padding.
+payload = json.loads(base64.urlsafe_b64decode(words[1] + '==')) # Kludge for incorrect padding.
 print(json.dumps(payload, indent=4))
 
 if 'iat' in payload:
